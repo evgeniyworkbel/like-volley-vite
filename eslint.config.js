eff95734c9ed3e2dee184670dae13b8640eb8220
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-17 01:31:22 +0300

    build(packages): install eslint plugin to check use-effects, enable rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactUseEffect from "eslint-plugin-react-useeffect";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
@@ -10,6 +11,9 @@ export default tseslint.config([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-useeffect": reactUseEffect,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,6 +22,7 @@ export default tseslint.config([
       eslintConfigPrettier,
     ],
     rules: {
+      "react-useeffect/no-non-function-return": "error",
       "@typescript-eslint/consistent-type-definitions": ["error", "type"],
     },
     languageOptions: {
